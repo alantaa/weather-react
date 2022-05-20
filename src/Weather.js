cc9565eaade7994c1cc884d2d4bcc3768dcd4951
alantaa <102055628+alantaa@users.noreply.github.com>
2022-05-20 22:38:37 +0100

    Added some key styling to work on

--- src/Weather.js
+++ src/Weather.js
@@ -9,22 +9,38 @@ export default function Weather() {
   );
   return (
     <div className="Weather">
-      <div className="row">
-        <div className="col-6">
+      <div className="row justify-content-between">
+        <div className="col-2 list1">
           <ul>
-            <li>Icon</li>
+            <li>Icon </li>
             <li>Description</li>
             <li>London</li>
             <li>20C</li>
             <li>{form}</li>
           </ul>
         </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity</li>
-            <li>Wind Speed</li>
-            <li>Air Pressure</li>
-            <li>Chance of rain</li>
+        <div className="col-2 list2">
+          <ul className="list2">
+            <li>
+              Humidity
+              <br />
+              <strong>50%</strong>
+            </li>
+            <li>
+              Wind Speed <br />
+              <strong>5m/m</strong>
+            </li>
+
+            <li>
+              Air Pressure
+              <br />
+              <strong>50PS</strong>
+            </li>
+            <li>
+              Chance of Rain
+              <br />
+              <strong>5%</strong>
+            </li>
           </ul>
         </div>
       </div>
