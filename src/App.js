f0fb2d5e61d679e2d20fe1d3e13912b650ea24bb
alantaa <102055628+alantaa@users.noreply.github.com>
2022-05-20 17:53:12 +0100

    Added github repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <h1>Weather App</h1>
+      <footer className="App-footer">
+        Coded by{" "}
+        <a
+          href="https://extraordinary-stroopwafel-905036.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Alanta
+        </a>
+        , open-sourced on{" "}
+        <a
+          href="https://github.com/alantaa/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </footer>
     </div>
   );
 }
